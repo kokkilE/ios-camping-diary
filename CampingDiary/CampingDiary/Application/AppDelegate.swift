6ccf94d5130724e9e1a3dc102acf73f8043855cc
kkuldak1@naver.com <kkuldak1@naver.com>
2023-11-06 14:28:18 +0900

    chore: 프로젝트 초기 설정
    스토리보드 삭제, 기본 폴더 구조 생성

---
+++ CampingDiary/CampingDiary/Application/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  CampingDiary
+//
+//  Created by 조향래 on 11/5/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}
---
+++ CampingDiary/CampingDiary/Application/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  CampingDiary
+//
+//  Created by 조향래 on 11/5/23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+
+        window = UIWindow(windowScene: windowScene)
+        
+        let viewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+}
