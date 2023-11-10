f7e79a19893696b66b1d67ebe4bcb715613371fc
kkuldak1@naver.com <kkuldak1@naver.com>
2023-11-10 16:56:04 +0900

    feat: 홈 화면에 저장된 캠핑 일지를 표시하기 위한 cell 구현

---
+++ CampingDiary/CampingDiary/HomeView/DiaryCollectionViewCell.swift
@@ -0,0 +1,65 @@
+//
+//  DiaryCollectionViewCell.swift
+//  CampingDiary
+//
+//  Created by 조향래 on 11/10/23.
+//
+
+import UIKit
+import RxSwift
+
+final class DiaryCollectionViewCell: UICollectionViewCell {
+    private lazy var mainStackView = {
+        let stackView = UIStackView(arrangedSubviews: [titleLabel, imageView])
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return stackView
+    }()
+    private let titleLabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.numberOfLines = 1
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.5
+        
+        return label
+    }()
+    private let imageView = {
+        let image = UIImageView()
+        
+        return image
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        addSubviews()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addSubviews() {
+        contentView.addSubview(mainStackView)
+    }
+    
+    private func layout() {
+        let safe = contentView.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            mainStackView.topAnchor.constraint(equalTo: safe.topAnchor, constant: 8),
+            mainStackView.leadingAnchor.constraint(equalTo: safe.leadingAnchor, constant: 8),
+            mainStackView.trailingAnchor.constraint(equalTo: safe.trailingAnchor, constant: -8),
+            mainStackView.bottomAnchor.constraint(equalTo: safe.bottomAnchor, constant: -8)
+        ])
+    }
+    
+    func configure(title: String, image: UIImage?) {
+        titleLabel.text = title
+        imageView.image = image
+    }
+}
